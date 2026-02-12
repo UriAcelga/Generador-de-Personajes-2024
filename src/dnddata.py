1800c1af67ff90a3d88335e20654ef699a56378c
UriAcelga <urigomez2002@gmail.com>
2026-02-12 14:36:02 -0300

    componentes básicos de formulario (falta implementar)

--- src/dnddata.py
+++ src/dnddata.py
@@ -1,36 +1,73 @@
 class DndData:
     @staticmethod
-    def get_clases():
+    def clases():
         return [
-            "Barbaro",
+            "Bárbaro",
             "Bardo",
             "Brujo",
-            "Clerigo",
+            "Clérigo",
             "Druida",
             "Explorador",
             "Guerrero",
             "Hechicero",
             "Mago",
             "Monje",
-            "Paladin",
-            "Picaro"
+            "Paladín",
+            "Pícaro"
         ]
     
     @staticmethod
-    def get_trasfondos():
+    def razas():
+        return [
+            "Aasimar"
+            "Dracónido", 
+            "Elfo", 
+            "Enano", 
+            "Gnomo", 
+            "Goliat", 
+            "Humano", 
+            "Mediano", 
+            "Orco", 
+            "Tiefling"
+        ]
+
+    @staticmethod
+    def trasfondos():
+        return [
+            "Acólito",
+            "Animador",
+            "Artesano",
+            "Campesino",
+            "Charlatán",
+            "Comerciante",
+            "Criminal",
+            "Ermitaño",
+            "Erudito",
+            "Escriba",
+            "Guardia",
+            "Guía",
+            "Marinero",
+            "Noble",
+            "Soldado",
+            "Vagabundo"
+
+        ]        
+    
+    @staticmethod
+    def trasfondos_por_clase():
         return {
-            "Barbaro": ["Granjero", "Guía", "Soldado", "Marinero"],
-            "Bardo": ["Charlatán", "Artista", "Noble", "Caminante"],
-            "Brujo": ["Acólito", "Charlatán", "Ermitaño", "Mercader"],
-            "Clerigo": ["Acólito", "Ermitaño", "Sabio", "Escribano"],
-            "Druida": ["Guía", "Ermitaño", "Erudito", "Escribano", "Caminante"],
-            "Explorador": ["Guía", "Criminal", "Caminante"],
-            "Guerrero": ["Soldado", "Noble", "Guardia", "Granjero"],
-            "Hechicero": ["Charlatán", "Ermitaño", "Sabio", "Acólito"],
-            "Mago": ["Acólito", "Escribano", "Ermitaño", "Mercader"],
+            "Barbaro": ["Campesino", "Guía", "Soldado", "Marinero"],
+            "Bardo": ["Charlatán", "Animador", "Noble", "Vagabundo"],
+            "Brujo": ["Acólito", "Charlatán", "Ermitaño", "Comerciante"],
+            "Clerigo": ["Acólito", "Ermitaño", "Erudito", "Escriba"],
+            "Druida": ["Guía", "Ermitaño", "Erudito", "Escriba", "Vagabundo"],
+            "Explorador": ["Guía", "Criminal", "Vagabundo"],
+            "Guerrero": ["Soldado", "Noble", "Guardia", "Campesino"],
+            "Hechicero": ["Charlatán", "Ermitaño", "Erudito", "Acólito"],
+            "Mago": ["Acólito", "Escriba", "Ermitaño", "Comerciante"],
             "Monje": ["Guía", "Marinero"],
             "Paladin": ["Noble", "Soldado", "Acólito"],
-            "Picaro": ["Criminal", "Charlatán", "Caminante"]
+            "Picaro": ["Criminal", "Charlatán", "Vagabundo"]
         }
     
     
--- src/main.py
+++ src/main.py
@@ -3,34 +3,56 @@ from flet import Colors
 from pdfmanager import PdfManager
 from personaje import Personaje
 
+
+def Dropdown(default="Selecciona una opción", dp_label="Seleccion", opciones=["1", "2", "3"]):
+    dp_opciones = [ft.DropdownOption(opcion) for opcion in opciones]
+    def dp_on_change(e):
+        print(f"Selección de clase: {e.control.value}")
+        e.control.update()
+        
+    return ft.Dropdown(
+        label=dp_label,
+        options=dp_opciones,
+        on_select=dp_on_change
+    )
+
+def Slider(label="Selección:", min=1, max=20):
+    valor_slider = min
+    slider_text = ft.Text(value=f"Valor actual: {valor_slider}")
+    def cambiar_slider(e):
+        valor_slider = int(e.control.value)
+        slider_text.value = f"Valor actual: {valor_slider}"
+        slider_text.update()
+
+    return ft.Column(
+            controls=[
+                ft.Text(label),
+                ft.Slider(
+                    value=valor_slider,
+                    min=min,
+                    max=max,
+                    divisions=max-min,
+                    label="{value}",
+                    on_change=cambiar_slider,
+                ),
+                slider_text,
+            ]
+        )
+
+
+
 def main(page: ft.Page):
 
-
-    status_text = ft.Text("Presiona el botón para editar el PDF modelo", size=16)
+    status_text = ft.Text(
+        "Presiona el botón para editar el PDF modelo", size=16)
     tiradas_text = ft.Text("Generar tiradas", size=16)
 
-    input_nombre = ft.TextField(
-        label="Nombre de personaje",
-        hint_text="Gandalf",
-        expand=True
-    )
-    select_clase = ft.Dropdown(
-        label="Clase",
-        options=[],
-    )
-    llenar_dropdown(select_clase, )
-
-    def llenar_dropdown(dropdown, opciones):
-        for opcion in opciones:
-            dropdown.options.append(opcion)
-    
     def generar_personaje(e):
         data_pj = {"NVL": 3, "CLASE": "Guerrero"}
         pj = Personaje(data=data_pj)
         tiradas_text.value = f"✅ Tiradas: {pj.tiradas}"
         tiradas_text.color = ft.Colors.GREEN
         page.update()
-
 
     def gestionar_pdf(e):
         pdfm = PdfManager()
@@ -41,9 +63,10 @@ def main(page: ft.Page):
             path_resultado = pdfm.crear_anotaciones()
             status_text.value = f"✅ PDF guardado en: {path_resultado}"
             status_text.color = ft.Colors.GREEN
-            
+
             # Notificación visual rápida
-            page.snack_bar = ft.SnackBar(ft.Text("¡Proceso completado con éxito!"))
+            page.snack_bar = ft.SnackBar(
+                ft.Text("¡Proceso completado con éxito!"))
             page.snack_bar.open = True
             """try:
             except Exception as ex:
@@ -53,9 +76,10 @@ def main(page: ft.Page):
             status_text.value = "❌ No se encontró el pdf"
             status_text.color = ft.Colors.ORANGE
             print("Status:", pdfm.get_status())
-        
+
         page.update()
 
+    # Renderizado de componentes gráficos
     page.add(
         ft.Row(
             alignment=ft.MainAxisAlignment.CENTER,
@@ -70,13 +94,23 @@ def main(page: ft.Page):
                 tiradas_text,
                 ft.IconButton(ft.Icons.ADD, on_click=generar_personaje),
             ],
-        ),    
+        ),
         ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
             controls=[
-                input_nombre,
-                select_clase,
+                Dropdown(dp_label="Clase:", opciones=["barbaro", "mago", "paladin"]),
+                ft.TextField(
+                    label="Nombre de personaje",
+                    hint_text="Gandalf",
+                    expand=True
+                ),
             ]
-        ) 
+        ),
+        ft.Column(
+            Slider(label="Nivel:", min=1, max=20)
+        ),
+
+        
     )
 
 
